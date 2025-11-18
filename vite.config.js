3dd3b7846ba983fc364d56115c536cf475babb1b
DiogoBAguiar <136986672+DiogoBAguiar@users.noreply.github.com>
2025-11-18 17:26:59 -0300

    Corrigindo base path para caminhos relativos

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: '/',
+  base: './',
   // Raiz continua sendo os templates HTML
   root: path.resolve(__dirname, 'src/templates'),
   
